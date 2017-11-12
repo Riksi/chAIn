97a6f7a195b92d0f9b2fdcb5bd69e8512be71fd7
Nikhila Ravi <nikkiravi92@gmail.com>
2017-11-12 11:49:06 -0800

    Updates to company and ownder

--- company.py
+++ company.py
@@ -10,6 +10,7 @@ from syft.he.paillier.keys import KeyPair
 from syft.nn.linear import LinearClassifier
 from sklearn.datasets import load_diabetes
 from threading import Thread
+import os
 
 app = Flask(__name__)
 
@@ -21,16 +22,17 @@ ADDRESS = '0x345ca3e014aaf5dca488057592ee47305d9b3e10'
 PORT = 9545
 HOST = 'localhost'
 
-REPO = ModelRepository(contract_address = ADDRESS, 
+REPO = ModelRepository(contract_address = ADDRESS,
 web3_port = PORT, web3_host = HOST)
 
 CLASSIFIERS = {'Linear_Classifier': LinearClassifier}
 names = {}
-num_gradients = {}
+num_gradients = {0:0}
 errors = {}
 data = {}
 address = 1
 pubkey,prikey = KeyPair().generate(n_length=1024)
+company_owner ='0xf17f52151ebef6c7334fad080c5704d77216b732'
 
 def get_gradients(model_id):
     num_grads_old = num_gradients[model_id]
@@ -38,16 +40,20 @@ def get_gradients(model_id):
     grads = []
     if num_grads_new - num_grads_old > 0:
         num_gradients[model_id] = num_grads_new
-        #Since id is incremented from 0 
+        #Since id is incremented from 0
         for grad_id in range(num_grads_old, num_grads_new):
             grads.append(REPO[model_id][grad_id])
     return grads
 
 def evaluate_gradients(model_id):
     grads = get_gradients(model_id)
+    input_path = os.path.join(UPLOAD_FOLDER, 'input_data')
+    target_path = os.path.join(UPLOAD_FOLDER, 'target_data')
+    input_data = pd.read_csv(input_path).values
+    target_data = pd.read_csv(target_path).values
     if len(grads):
         for grad in grads:
-            new_error = REPO[model_id].evaluate_gradient(address,grad,prikey,pubkey,data[model_id]['input'],data[model_id]['target'])
+            new_error = REPO[model_id].evaluate_gradient(company_owner,grad,prikey,pubkey,input_data,target_data)
             errors[model_id].append(new_error)
 
 # def manage_gradients():
@@ -69,9 +75,11 @@ def get_model_details():
 
 @app.route('/notify')
 def notify():
-    model_id = request.args.get('model_id')
+    model_id = int(request.args.get('model_id'))
+    print('model_id', model_id)
     if model_id is not None:
         evaluate_gradients(model_id)
+    return "done"
 
 @app.route('/add_model',methods=['POST'])
 def add_model():
@@ -83,14 +91,14 @@ def add_model():
     input_data = request.files['input_data']
     target_data = request.files['target_data']
 
-    input_path = os.path.join(app.config['UPLOAD_FOLDER'], 'input_data')
-    target_path = os.path.join(app.config['UPLOAD_FOLDER'], 'target_data')
+    input_path = os.path.join(UPLOAD_FOLDER, 'input_data')
+    target_path = os.path.join(UPLOAD_FOLDER, 'target_data')
 
     input_data.save(input_path)
     target_data.save(target_path)
 
-    input_data = pd.read_csv(input_path)
-    target_data = pd.read_csv(target_path)
+    input_data = pd.read_csv(input_path).values
+    target_data = pd.read_csv(target_path).values
 
     # input_data = request.form.get('input_data').split('\n')
     # target_data = request.form.get('target_data').split('\n')
@@ -103,18 +111,21 @@ def add_model():
                                 n_labels=target_data.shape[1])
     initial_error = clf.evaluate(input_data, target_data)
     clf.encrypt(pubkey)
-    model = Model(owner=address,
+    model = Model(owner=company_owner,
                 syft_obj = clf,
-                bounty = bounty,
+                bounty = int(bounty),
                 initial_error = initial_error,
-                target_error = target_error
+                target_error = int(target_error)
                 )
     model_id = REPO.submit_model(model)
     num_gradients[model_id] = 0
     errors[model_id] = []
     names[model_id] = model_name
+
+    data[model_id] = {}
     data[model_id]['input'] = input_data
     data[model_id]['target'] = target_data
+    return jsonify({'model_name':model_name})
 
 @app.route('/dashboard')
 def get_model_data():
@@ -125,4 +136,4 @@ def get_model_data():
     return jsonify(data)
 
 app.debug = True
-app.run(port=5555, host='0.0.0.0')
+app.run(port=5556, host='0.0.0.0')
--- data_owner.py
+++ data_owner.py
@@ -8,6 +8,8 @@ from sonar.contracts import ModelRepository,Model
 from syft.he.paillier.keys import KeyPair
 from syft.nn.linear import LinearClassifier
 from sklearn.datasets import load_diabetes
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -15,14 +17,16 @@ balances = []
 
 PUBLIC_KEY = 1
 
-NOTIFY_URL = 'nfy'
-MODELS_URL = 'mdl'
+NOTIFY_URL = 'http://localhost:5556/notify'
+MODELS_URL = 'http://localhost:5556/models'
 
 CONTRACT_ADDRESS = '0x345ca3e014aaf5dca488057592ee47305d9b3e10'
 PORT = 9545
 HOST = 'localhost'
 
-REPO = ModelRepository(contract_address = CONTRACT_ADDRESS, 
+UPLOAD_FOLDER = 'uploads'
+
+REPO = ModelRepository(contract_address = CONTRACT_ADDRESS,
     web3_port = PORT, web3_host = HOST)
 
 def get_balance(account):
@@ -31,20 +35,33 @@ def get_balance(account):
 @app.route('/dashboard', methods=['POST'])
 def share_data():
     balances.append(get_balance(PUBLIC_KEY))
-    address = request.form.get('address')
-    model_id = request.form.get('model_id')
-    input_data = request.form.get('input_data')
-    target_data = request.form.get('target_data')
+    model_id = int(request.form.get('model_id'))
+    print('model_id', model_id)
+    data_owner_address = request.form.get('data_owner_address')
+    input_data = request.files['input_data']
+    target_data = request.files['target_data']
+
+    input_path = os.path.join(UPLOAD_FOLDER, 'input_data')
+    target_path = os.path.join(UPLOAD_FOLDER, 'target_data')
+
+    input_data.save(input_path)
+    target_data.save(target_path)
+
+    input_data = pd.read_csv(input_path).values
+    target_data = pd.read_csv(target_path).values
+
     model = REPO[model_id]
-    model.submit_gradient(address,input_data,target_data)
+
+    model.submit_gradient(data_owner_address,input_data[0],target_data[0])
+    requests.get(NOTIFY_URL, params={'model_id':model_id})
     balances.append(get_balance(PUBLIC_KEY))
     return jsonify({'balances':balances})
 
 @app.route('/dashboard')
 def get_results():
     models = requests.get(MODELS_URL).json()
-    return jsonify({'balances':balances, 
+    return jsonify({'balances':balances,
                     'models':models})
 
 app.debug = True
-app.run(port=5555, host='0.0.0.0')
+app.run(port=5557, host='0.0.0.0')
